f224c5425be319aad7307a5fe5f76b52021454e4
umaru <15875339926@139.com>
2025-11-10 20:03:15 +0800

    feat: introduce and integrate new Shell tool functionality
    - Add a new Shell tool and integrate it into the tools system
    - Implement the `shell_execute` function to execute shell commands with specific restrictions and guidelines
    - Update the tools loader to include the new Shell tool
    - Modify tests to include testing for the new Shell tool server functionality
    - Extend test prompts to include a new time-related query
    
    Signed-off-by: umaru <15875339926@139.com>

--- neco/tests/llm/agent/react_agent_test.py
+++ neco/tests/llm/agent/react_agent_test.py
@@ -5,14 +5,15 @@ import pytest
 from loguru import logger
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
-NEW_API_KEY=os.getenv('TEST_LLM_API_KEY')
-NEW_API_URL=os.getenv('TEST_LLM_API_URL')
-TEST_LLM_MODEL=os.getenv('TEST_LLM_MODEL')
+NEW_API_KEY = os.getenv('TEST_LLM_API_KEY')
+NEW_API_URL = os.getenv('TEST_LLM_API_URL')
+TEST_LLM_MODEL = os.getenv('TEST_LLM_MODEL')
 
 TEST_PROMPT = [
     # '分析https://github.com/TencentBlueKing/bk-lite仓库今天的提交情况',
-    '现在几点'
+    '现在是哪个时区，几点'
 ]
+
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize('prompt', TEST_PROMPT)
@@ -32,12 +33,15 @@ async def test_react_agent(prompt):
             ToolsServer(
                 name='github',
                 url='langchain:github'
+            ),
+            ToolsServer(
+                name='shell',
+                url='langchain:shell'
             )
         ],
     )
-    graph=ReActAgentGraph()
+    graph = ReActAgentGraph()
     result = await graph.stream(request)
     async for chunk in result:
         content = await graph.filter_messages(chunk)
         print(content, end='', flush=True)
-
