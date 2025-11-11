c487c2267e558043603fc8c705e81cfeab0827b1
windyzhao <1129710372@qq.com>
2025-11-11 17:45:46 +0800

    feature-运营分析权限接入

--- server/apps/core/utils/permission_utils.py
+++ server/apps/core/utils/permission_utils.py
@@ -23,6 +23,7 @@ def set_rules_module_params(app_name, permission_key):
         "node_mgmt": "node",
         "console_mgmt": "ops-console",
         "mlops": "mlops",
+        "operation_analysis": "ops-analysis"
     }
     client = SystemMgmt(is_local_client=True)
     app_name = app_name_map.get(app_name, app_name)
@@ -40,6 +41,7 @@ def get_permissions_rules(user, current_team, app_name, permission_key, include_
         "node_mgmt": "node",
         "console_mgmt": "ops-console",
         "mlops": "mlops",
+        "operation_analysis": "ops-analysis"
     }
     app_name = app_name_map.get(app_name, app_name)
     module = permission_key
