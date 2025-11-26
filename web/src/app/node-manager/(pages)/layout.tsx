8fcb89c99f3cc9be58ea6d593260ae180f490806
CharlesWhj <1253297146@qq.com>
2025-11-26 09:39:10 +0800

    optimization:The enumerated interface becomes a public interface, shared through context

--- web/src/app/node-manager/(pages)/layout.tsx
+++ web/src/app/node-manager/(pages)/layout.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import CommonProvider from '@/app/node-manager/context/common';
 import '@/app/node-manager/styles/index.css';
+import useApiClient from '@/utils/request';
 
 export default function RootMonitor({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  const { isLoading } = useApiClient();
+  return <CommonProvider>{isLoading ? null : children}</CommonProvider>;
 }
