2fb23900a3fa1be9b4f1dcef67ec8d7cedb49101
676934036@qq.com <luoyang@luoyangdeMacBook-Air.local>
2025-12-12 17:42:08 +0800

    fix：inst_name获取方式不能写死

--- server/apps/cmdb/collection/collect_plugin/base.py
+++ server/apps/cmdb/collection/collect_plugin/base.py
@@ -23,7 +23,8 @@ class CollectBase(metaclass=ABCMeta):
         self.inst_name = inst_name
         if not self.inst_name:
             task_inst_data = self.get_collect_inst().instances
-            self.inst_name = task_inst_data[0]['inst_name']
+            if task_inst_data.__len__():
+                self.inst_name = task_inst_data[0]['inst_name']
         assert self.check_metrics(), "请定义_metrics"
         self.collection_metrics_dict = {i: [] for i in self._metrics}
         self.timestamp_gt = False
