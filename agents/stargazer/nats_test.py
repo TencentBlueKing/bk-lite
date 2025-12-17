d700942e2bd63e21f1ff12abf3045b43456bb59a
windyzhao <1129710372@qq.com>
2025-12-17 15:04:43 +0800

    feature-Stargazer 主机采集优化逻辑之下nats脚本前先验证是否存在此主机

---
+++ agents/stargazer/nats_test.py
@@ -0,0 +1,31 @@
+# -- coding: utf-8 --
+# @File: nats_test.py
+# @Time: 2025/12/16 16:12
+# @Author: windyzhao
+"""
+测试 NATS 通用工具方法
+如接口 bklite.node_list
+"""
+import asyncio
+import json
+
+from dotenv import load_dotenv
+
+from core.nats_utils import nats_request
+
+
+async def run_test():
+    subject = "bklite.node_list"
+    # 使用正确的 exec_params 格式
+    exec_params = {
+        "args": [{"ip": "10.10.41.14"}],  # 位置参数列表
+        "kwargs": {}  # 关键字参数字典
+    }
+    payload = json.dumps(exec_params).encode()
+    response = await nats_request(subject, payload, timeout=10.0)
+    print("NATS Response:", response)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    asyncio.run(run_test())
