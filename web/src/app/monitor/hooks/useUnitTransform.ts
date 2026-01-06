056ab232bd73ed45dbdca579c4f3654ecf4e93ff
CharlesWhj <1253297146@qq.com>
2026-01-06 11:39:36 +0800

    minor: code optimization

--- web/src/app/monitor/hooks/useUnitTransform.ts
+++ web/src/app/monitor/hooks/useUnitTransform.ts
@@ -9,14 +9,12 @@ export const useUnitTransform = () => {
   const unitList = commonContext?.unitList || [];
 
   const findUnitNameById = (value: unknown): string => {
-    if (!value || value === 'short') return '';
-    if (isStringArray(value as string)) {
+    if (!value || value === 'short' || isStringArray(value as string)) {
       return '';
     }
     const unit = unitList.find((item) => item.unit_id === value);
-    return unit?.display_unit === 'short'
-      ? ''
-      : unit?.display_unit || value?.toString() || '';
+    const isShortUnit = unit?.display_unit === 'short';
+    return isShortUnit ? '' : unit?.display_unit || value?.toString() || '';
   };
 
   const getEnumValueUnit = (metric: MetricItem, id: number | string) => {
