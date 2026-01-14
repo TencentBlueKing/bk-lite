bc7fc4ec7791172818347aa5a1d32ae08495d838
CharlesWhj <1253297146@qq.com>
2026-01-14 14:32:18 +0800

    bugfix: There are no type restrictions for secret key uploads

--- web/src/app/node-manager/(pages)/cloudregion/node/controllerUninstall.tsx
+++ web/src/app/node-manager/(pages)/cloudregion/node/controllerUninstall.tsx
@@ -207,7 +207,6 @@ const ControllerUninstall = forwardRef<ModalRef, ModalSuccess>(
                       onClick={() => {
                         const input = document.createElement('input');
                         input.type = 'file';
-                        input.accept = '.*';
                         input.onchange = (e: any) => {
                           const file = e.target.files[0];
                           if (file) {
