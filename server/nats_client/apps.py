17bdaadeda78879cf22eacff39cf7bc7a858b525
baiyufei <baiyfyx@163.com>
2026-03-12 16:10:07 +0800

    Revert "feat: ansible回调测试"
    This reverts commit 39e6ba61d09043bf24e81ef67ae2f20e628ded14.

--- server/nats_client/apps.py
+++ server/nats_client/apps.py
@@ -2,8 +2,4 @@ from django.apps import AppConfig
 
 
 class DjangoNatsConfig(AppConfig):
-    name = "nats_client"
-
-    def ready(self):
-        # register rpc-level callback handlers for tests
-        import apps.rpc.ansible  # noqa
+    name = 'nats_client'
