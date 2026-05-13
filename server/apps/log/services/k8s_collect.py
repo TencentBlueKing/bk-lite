c7d15d76dc669d34d1a12cd33373c5fc94f5130d
CharlesWhj <1253297146@qq.com>
2026-05-13 15:55:51 +0800

    bugfix: Fix the issue with the time format of the verification interface for k8s log access

--- server/apps/log/services/k8s_collect.py
+++ server/apps/log/services/k8s_collect.py
@@ -263,8 +263,8 @@ class K8sLogCollectService:
         query = f'collect_type:"kubernetes" AND instance_id:"{instance.id}"'
         data = SearchService.search_logs(
             query,
-            start_time.strftime("%Y-%m-%d %H:%M:%S"),
-            end_time.strftime("%Y-%m-%d %H:%M:%S"),
+            start_time.isoformat(timespec="seconds"),
+            end_time.isoformat(timespec="seconds"),
             1,
         )
         return bool(data)
