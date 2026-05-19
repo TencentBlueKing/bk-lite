9960f94548146621dfcd7916cb2ce548daec80e7
zhongming Fan <zhongmingfan@canway.net>
2026-05-19 15:44:30 +0800

    fix(monitor): align Dameng instance list metrics

--- web/src/app/monitor/hooks/integration/objects/database/dm.tsx
+++ web/src/app/monitor/hooks/integration/objects/database/dm.tsx
@@ -3,11 +3,8 @@ export const useDmConfig = () => {
     instance_type: 'dameng',
     dashboardDisplay: [],
     tableDiaplay: [
-      { type: 'value', key: 'dm_exporter_connections_gauge' },
       { type: 'progress', key: 'dm_exporter_session_used_ratio_gauge' },
       { type: 'progress', key: 'dm_exporter_tablespace_used_ratio_gauge' },
-      { type: 'value', key: 'dm_exporter_lock_blocks_gauge' },
-      { type: 'value', key: 'dm_exporter_slow_query_gauge' },
     ],
     groupIds: {},
     collectTypes: {
