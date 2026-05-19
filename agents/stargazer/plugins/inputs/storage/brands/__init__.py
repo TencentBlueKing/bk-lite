8b5398697f959d4cb914f553df9f112781d2c65b
baiyufei <baiyfyx@163.com>
2026-05-19 16:25:14 +0800

    Revert "feat(stargazer): add storage plugin with OceanStor brand support"
    This reverts commit a67f99e200da52caab32e831c554c70276626331.

--- agents/stargazer/plugins/inputs/storage/brands/__init__.py
+++
@@ -1,9 +0,0 @@
-from plugins.inputs.storage.brands.oceanstor import OceanStorBrandCollector
-
-BRAND_REGISTRY = {
-    "oceanstor": OceanStorBrandCollector,
-}
-
-
-def get_brand_collector(brand: str):
-    return BRAND_REGISTRY.get(brand)
