0bf9d0686bc19175ea6308102ab3f6727b2e83fa
windyzhao <1129710372@qq.com>
2026-06-04 10:51:55 +0800

    bugfix-修复celery beat无法启动的问题

--- server/apps/core/celery.py
+++ server/apps/core/celery.py
@@ -10,7 +10,7 @@ from celery.schedules import crontab
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 app = Celery("bklite")
-app.config_from_object("django.conf:settings")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
 
