260f82fd819c63137004e448dcea8c71db8fe1df
zhongming Fan <zhongmingfan@canway.net>
2026-06-24 18:17:13 +0800

    test(monitor): add Nokia switch plugin contract tests + wire switch.tsx mapping

--- web/src/app/monitor/hooks/integration/objects/networkDevice/switch.tsx
+++ web/src/app/monitor/hooks/integration/objects/networkDevice/switch.tsx
@@ -126,6 +126,7 @@ export const useSwitchConfig = () => {
       'Switch Datacom SNMP': 'snmp_datacom',
       'Switch Eltex SNMP': 'snmp_eltex',
       'Switch SNR SNMP': 'snmp_snr',
+      'Switch Nokia SNMP': 'snmp_nokia',
       'Switch Parks SNMP': 'snmp_parks',
       'Switch Ubiquiti SNMP': 'snmp_ubiquiti',
       'Switch Ruijie SNMP': 'snmp_ruijie',
