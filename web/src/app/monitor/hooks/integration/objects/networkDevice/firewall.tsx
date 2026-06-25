eca7f3a4e9843da195d7a6dc726a146ced9c976e
ZhongmingFan <46909728+ZhongmingFan@users.noreply.github.com>
2026-06-25 13:38:14 +0800

    feat(monitor): add BDCOM/Korenix/Neteye/Bluedon/Harbour SNMP plugins (#3729)
    * feat(monitor): add BDCOM Switch SNMP plugin
    
    (cherry picked from commit 07fd4ead39ed19602ffbd0742e6bb6baba16e83f)
    
    * test(monitor): add BDCOM switch plugin contract tests
    
    (cherry picked from commit d5762fa4b23a885b6d828a74a44cd9ab9e5677d0)
    
    * feat(monitor): add Korenix Switch SNMP plugin
    
    (cherry picked from commit 8f5d8d3d4e5240c06ddbe75b53e2e4b46a4026ee)
    
    * test(monitor): add Korenix switch SNMP plugin contract tests
    
    (cherry picked from commit 90d96cb526e95c34aa06d23261f824d540a81471)
    
    * feat(monitor): add Neteye firewall SNMP plugin
    
    (cherry picked from commit eb505f96cef97aa6e54cafc01e0eb3171dec4bc6)
    
    * feat(monitor): add Bluedon firewall SNMP plugin
    
    (cherry picked from commit 3179a39556e9a6be29538aaac6c30ae8b61af281)
    
    * feat(monitor): add Harbour Networks router SNMP plugin
    
    (cherry picked from commit 02c002edca87c9f1bacfbb6f3c2b677cbb2626d4)
    
    * test(monitor): add Neteye/Bluedon/Harbour SNMP plugin contract tests
    
    (cherry picked from commit 1c63902d02446d9ad5fa2bed1df61f6f07f27435)
    
    * fix(monitor): scale Harbour router memory total/free from kBytes to bytes
    
    (cherry picked from commit 7618819686e655b658d12bae75430ae76851fdc2)
    
    * fix(monitor): add .0 scalar suffix to Bluedon firewall health OIDs
    
    ---------
    
    Co-authored-by: zhongming Fan <zhongmingfan@canway.net>

--- web/src/app/monitor/hooks/integration/objects/networkDevice/firewall.tsx
+++ web/src/app/monitor/hooks/integration/objects/networkDevice/firewall.tsx
@@ -63,6 +63,8 @@ export const useFirewallConfig = () => {
       'Firewall Sophos XG SNMP': 'snmp_sophos',
       'Firewall Forcepoint SNMP': 'snmp_forcepoint',
       'Firewall ScreenOS SNMP': 'snmp_screenos',
+      'Firewall Neteye SNMP': 'snmp_neteye',
+      'Firewall Bluedon SNMP': 'snmp_bluedon',
       'Firewall Check Point SNMP': 'snmp_checkpoint',
       'Firewall Stormshield SNMP': 'snmp_stormshield',
       'Firewall Palo Alto SNMP': 'snmp_paloalto',
