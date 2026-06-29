cceeddf553c28b8b9f2fe2cdc40ef4f38f9d9c2f
zhongming Fan <zhongmingfan@canway.net>
2026-06-29 09:46:56 +0800

    feat(monitor): add Sixnet switch SNMP plugin

--- web/src/app/monitor/hooks/integration/objects/networkDevice/switch.tsx
+++ web/src/app/monitor/hooks/integration/objects/networkDevice/switch.tsx
@@ -128,6 +128,7 @@ export const useSwitchConfig = () => {
       'Switch SNR SNMP': 'snmp_snr',
       'Switch Intelbras SNMP': 'snmp_intelbras',
       'Switch EtherWAN SNMP': 'snmp_etherwan',
+      'Switch Sixnet SNMP': 'snmp_sixnet',
       'Switch Red Lion SNMP': 'snmp_redlion',
       'Switch Alaxala SNMP': 'snmp_alaxala',
       'Switch Transition Networks SNMP': 'snmp_transition',
