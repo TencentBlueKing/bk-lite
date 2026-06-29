7291e9cddb92b0e4cffaf1c6b1a79996e9a09a41
zhongming Fan <zhongmingfan@canway.net>
2026-06-29 10:44:03 +0800

    fix(monitor): surface CPU/memory (and temperature for router) panels on router/firewall/loadbalance/networkService SNMP dashboards

--- web/src/app/monitor/hooks/integration/objects/networkDevice/router.tsx
+++ web/src/app/monitor/hooks/integration/objects/networkDevice/router.tsx
@@ -3,49 +3,70 @@ export const useRouterConfig = () => {
     instance_type: 'router',
     dashboardDisplay: [
       {
-        indexId: 'device_total_outgoing_traffic',
+        indexId: 'device_cpu_usage',
         displayType: 'single',
         sortIndex: 0,
         displayDimension: [],
         style: {
           height: '200px',
-          width: '15%'
+          width: '24%'
+        }
+      },
+      {
+        indexId: 'device_memory_usage',
+        displayType: 'single',
+        sortIndex: 1,
+        displayDimension: [],
+        style: {
+          height: '200px',
+          width: '24%'
+        }
+      },
+      {
+        indexId: 'device_total_incoming_traffic',
+        displayType: 'single',
+        sortIndex: 2,
+        displayDimension: [],
+        style: {
+          height: '200px',
+          width: '24%'
+        }
+      },
+      {
+        indexId: 'device_total_outgoing_traffic',
+        displayType: 'single',
+        sortIndex: 3,
+        displayDimension: [],
+        style: {
+          height: '200px',
+          width: '24%'
         }
       },
       {
         indexId: 'snmp_uptime',
         displayType: 'lineChart',
-        sortIndex: 1,
+        sortIndex: 4,
         displayDimension: [],
         style: {
           height: '200px',
-          width: '40%'
+          width: '49%'
         }
       },
       {
-        indexId: 'device_total_incoming_traffic',
+        indexId: 'device_temperature_celsius',
         displayType: 'lineChart',
-        sortIndex: 2,
-        displayDimension: [],
+        sortIndex: 5,
+        displayDimension: ['descr'],
         style: {
           height: '200px',
-          width: '40%'
+          width: '49%'
         }
       },
       {
         indexId: 'interfaces',
         displayType: 'multipleIndexsTable',
-        sortIndex: 3,
-        displayDimension: [
-          'ifOperStatus',
-          'ifHighSpeed',
-          'ifInErrors',
-          'ifOutErrors',
-          'ifInUcastPkts',
-          'ifOutUcastPkts',
-          'ifInOctets',
-          'ifOutOctets'
-        ],
+        sortIndex: 6,
+        displayDimension: ['ifOperStatus', 'ifHighSpeed', 'ifInErrors', 'ifOutErrors', 'ifInUcastPkts', 'ifOutUcastPkts', 'ifInOctets', 'ifOutOctets'],
         style: {
           height: '400px',
           width: '100%'
