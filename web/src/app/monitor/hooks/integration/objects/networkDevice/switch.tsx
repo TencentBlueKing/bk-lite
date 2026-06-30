98d3efc0b6981cc66d2ddcd098b3bb0669d99c96
zhongming Fan <zhongmingfan@canway.net>
2026-07-01 02:00:06 +0800

    feat(monitor): add WAGO switch SNMP plugin

--- web/src/app/monitor/hooks/integration/objects/networkDevice/switch.tsx
+++ web/src/app/monitor/hooks/integration/objects/networkDevice/switch.tsx
@@ -137,6 +137,7 @@ export const useSwitchConfig = () => {
       'Switch Waystream SNMP': 'snmp_waystream',
       'Switch TsnTec SNMP': 'snmp_tsntec',
       'Switch Antaira SNMP': 'snmp_antaira',
+      'Switch WAGO SNMP': 'snmp_wago',
       'Switch AsterFusion SNMP': 'snmp_asterfusion',
       'Switch Wi-Tek SNMP': 'snmp_witek',
       'Switch Nexans SNMP': 'snmp_nexans',
