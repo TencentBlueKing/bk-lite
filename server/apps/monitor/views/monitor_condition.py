640538ba9578eeb45284dc115e3c84f72b14e2ba
baiyufei <baiyfyx@163.com>
2026-07-07 16:39:58 +0800

    fix(monitor): 收口策略条件权限围栏

--- server/apps/monitor/views/monitor_condition.py
+++ server/apps/monitor/views/monitor_condition.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 
+from apps.core.exceptions.base_app_exception import BaseAppException, UnauthorizedException
 from apps.core.utils.permission_utils import get_permission_rules, permission_filter
+from apps.core.utils.user_group import normalize_user_group_ids
 from apps.core.utils.web_utils import WebUtils
 from apps.monitor.constants.database import DatabaseConstants
 from apps.monitor.constants.permission import PermissionConstants
@@ -10,9 +12,44 @@ from apps.monitor.models.monitor_condition import (
     MonitorConditionOrganization,
 )
 from apps.monitor.serializers.monitor_condition import MonitorConditionSerializer
+from apps.monitor.services.node_mgmt import InstanceConfigService
 from apps.monitor.utils.pagination import parse_page_params
 from config.drf.pagination import CustomPageNumberPagination
 from apps.core.utils.team_utils import get_current_team
+
+
+def _build_actor_context(request):
+    current_team = get_current_team(request)
+    if current_team in (None, ""):
+        raise BaseAppException("缺少 current_team 参数")
+    try:
+        current_team = int(current_team)
+    except (TypeError, ValueError):
+        raise BaseAppException("current_team 参数非法")
+
+    return {
+        "username": request.user.username,
+        "domain": request.user.domain,
+        "current_team": current_team,
+        "include_children": request.COOKIES.get("include_children", "0") == "1",
+        "is_superuser": request.user.is_superuser,
+        "group_list": normalize_user_group_ids(getattr(request.user, "group_list", [])),
+    }
+
+
+def _normalize_orgs(organizations):
+    try:
+        return {int(org) for org in (organizations or []) if org not in (None, "")}
+    except (TypeError, ValueError):
+        raise BaseAppException("组织参数非法")
+
+
+def _operate_only_permission(permission):
+    return {
+        **permission,
+        "team": permission.get("team", []),
+        "instance": [item for item in permission.get("instance", []) if isinstance(item, dict) and "Operate" in item.get("permission", [])],
+    }
 
 
 class MonitorConditionViewSet(viewsets.ModelViewSet):
@@ -21,28 +58,63 @@ class MonitorConditionViewSet(viewsets.ModelViewSet):
     filterset_class = MonitorConditionFilter
     pagination_class = CustomPageNumberPagination
 
-    def list(self, request, *args, **kwargs):
-        include_children = request.COOKIES.get("include_children", "0") == "1"
-        permission = get_permission_rules(
-            request.user,
-            get_current_team(request),
+    def _get_permission(self):
+        return get_permission_rules(
+            self.request.user,
+            get_current_team(self.request),
             "monitor",
             PermissionConstants.CONDITION_MODULE,
-            include_children=include_children,
+            include_children=self.request.COOKIES.get("include_children", "0") == "1",
         )
-        qs = permission_filter(
+
+    def _scope_queryset(self, queryset, permission):
+        permitted_qs = permission_filter(
             MonitorCondition,
             permission,
             team_key="organizations__organization__in",
             id_key="id__in",
         )
+        return queryset.filter(id__in=permitted_qs.values("id")).distinct()
 
-        queryset = self.filter_queryset(qs)
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        request = getattr(self, "request", None)
+        if request is None or request.user.is_superuser:
+            return queryset
+
+        permission = self._get_permission()
+        if getattr(self, "action", "") in {"update", "partial_update", "destroy"}:
+            permission = _operate_only_permission(permission)
+        return self._scope_queryset(queryset, permission)
+
+    def _get_authorized_scope_groups(self, actor_context):
+        if actor_context["is_superuser"]:
+            return None
+
+        groups = set(InstanceConfigService._get_actor_scope_groups(actor_context) or [])
+        if not groups:
+            raise UnauthorizedException("当前组织无可用权限范围")
+        return groups
+
+    def _ensure_target_organizations(self, organizations, actor_context=None):
+        target_orgs = _normalize_orgs(organizations)
+        if not target_orgs:
+            return
+
+        actor_context = actor_context or _build_actor_context(self.request)
+        if actor_context["is_superuser"]:
+            return
+
+        unauthorized_orgs = target_orgs - self._get_authorized_scope_groups(actor_context)
+        if unauthorized_orgs:
+            raise UnauthorizedException("无权限关联指定组织")
+
+    def list(self, request, *args, **kwargs):
+        permission = self._get_permission()
+        queryset = self.filter_queryset(self.get_queryset())
         queryset = queryset.distinct()
 
-        page, page_size = parse_page_params(
-            request.GET, default_page=1, default_page_size=10
-        )
+        page, page_size = parse_page_params(request.GET, default_page=1, default_page_size=10)
 
         start = (page - 1) * page_size
         end = start + page_size
@@ -52,9 +124,7 @@ class MonitorConditionViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(page_data, many=True)
         results = serializer.data
 
-        inst_permission_map = {
-            i["id"]: i["permission"] for i in permission.get("instance", [])
-        }
+        inst_permission_map = {i["id"]: i["permission"] for i in permission.get("instance", [])}
 
         for instance_info in results:
             if instance_info["id"] in inst_permission_map:
@@ -65,6 +135,7 @@ class MonitorConditionViewSet(viewsets.ModelViewSet):
         return WebUtils.response_success(dict(count=queryset.count(), items=results))
 
     def create(self, request, *args, **kwargs):
+        self._ensure_target_organizations(request.data.get("organizations", []))
         request.data["created_by"] = request.user.username
         response = super().create(request, *args, **kwargs)
         condition_id = response.data["id"]
@@ -73,8 +144,10 @@ class MonitorConditionViewSet(viewsets.ModelViewSet):
         return response
 
     def update(self, request, *args, **kwargs):
+        condition = self.get_object()
+        self._ensure_target_organizations(request.data.get("organizations", []))
         request.data["updated_by"] = request.user.username
-        condition_id = kwargs["pk"]
+        condition_id = condition.id
         response = super().update(request, *args, **kwargs)
         organizations = request.data.get("organizations", [])
         if organizations:
@@ -82,8 +155,11 @@ class MonitorConditionViewSet(viewsets.ModelViewSet):
         return response
 
     def partial_update(self, request, *args, **kwargs):
+        condition = self.get_object()
+        if "organizations" in request.data:
+            self._ensure_target_organizations(request.data.get("organizations", []))
         request.data["updated_by"] = request.user.username
-        condition_id = kwargs["pk"]
+        condition_id = condition.id
         response = super().partial_update(request, *args, **kwargs)
         organizations = request.data.get("organizations", [])
         if organizations:
@@ -91,32 +167,20 @@ class MonitorConditionViewSet(viewsets.ModelViewSet):
         return response
 
     def destroy(self, request, *args, **kwargs):
-        condition_id = kwargs["pk"]
-        MonitorConditionOrganization.objects.filter(
-            monitor_condition_id=condition_id
-        ).delete()
+        condition = self.get_object()
+        condition_id = condition.id
+        MonitorConditionOrganization.objects.filter(monitor_condition_id=condition_id).delete()
         return super().destroy(request, *args, **kwargs)
 
     def update_condition_organizations(self, condition_id, organizations):
         """更新条件的组织"""
-        old_organizations = MonitorConditionOrganization.objects.filter(
-            monitor_condition_id=condition_id
-        )
+        old_organizations = MonitorConditionOrganization.objects.filter(monitor_condition_id=condition_id)
         old_set = set([org.organization for org in old_organizations])
         new_set = set(organizations)
         # 删除不存在的组织
         delete_set = old_set - new_set
-        MonitorConditionOrganization.objects.filter(
-            monitor_condition_id=condition_id, organization__in=delete_set
-        ).delete()
+        MonitorConditionOrganization.objects.filter(monitor_condition_id=condition_id, organization__in=delete_set).delete()
         # 添加新的组织
         create_set = new_set - old_set
-        create_objs = [
-            MonitorConditionOrganization(
-                monitor_condition_id=condition_id, organization=org_id
-            )
-            for org_id in create_set
-        ]
-        MonitorConditionOrganization.objects.bulk_create(
-            create_objs, batch_size=DatabaseConstants.BULK_CREATE_BATCH_SIZE
-        )
+        create_objs = [MonitorConditionOrganization(monitor_condition_id=condition_id, organization=org_id) for org_id in create_set]
+        MonitorConditionOrganization.objects.bulk_create(create_objs, batch_size=DatabaseConstants.BULK_CREATE_BATCH_SIZE)
