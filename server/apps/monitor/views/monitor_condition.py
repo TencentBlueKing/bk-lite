5adfeef312cef3aab91e52567996fa49a49700de
justin <justinzhou@canway.net>
2026-09-16 12:51:23 +0800

    bugfix: 监控条件与组织关系改为同一事务

--- server/apps/monitor/views/monitor_condition.py
+++ server/apps/monitor/views/monitor_condition.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
@@ -101,11 +102,12 @@ class MonitorConditionViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         self._ensure_target_organizations(request.data.get("organizations", []))
         request.data["created_by"] = request.user.username
-        response = super().create(request, *args, **kwargs)
-        condition_id = response.data["id"]
-        organizations = request.data.get("organizations", [])
-        self.update_condition_organizations(condition_id, organizations)
-        return response
+        with transaction.atomic():
+            response = super().create(request, *args, **kwargs)
+            condition_id = response.data["id"]
+            organizations = request.data.get("organizations", [])
+            self.update_condition_organizations(condition_id, organizations)
+            return response
 
     def update(self, request, *args, **kwargs):
         if kwargs.get("partial", False):
@@ -115,10 +117,11 @@ class MonitorConditionViewSet(viewsets.ModelViewSet):
         self._ensure_target_organizations(request.data.get("organizations", []))
         request.data["updated_by"] = request.user.username
         condition_id = condition.id
-        response = super().update(request, *args, **kwargs)
-        organizations = request.data.get("organizations")
-        self.update_condition_organizations(condition_id, organizations)
-        return response
+        with transaction.atomic():
+            response = super().update(request, *args, **kwargs)
+            organizations = request.data.get("organizations")
+            self.update_condition_organizations(condition_id, organizations)
+            return response
 
     def partial_update(self, request, *args, **kwargs):
         condition = self.get_object()
@@ -126,18 +129,20 @@ class MonitorConditionViewSet(viewsets.ModelViewSet):
             self._ensure_target_organizations(request.data.get("organizations", []))
         request.data["updated_by"] = request.user.username
         condition_id = condition.id
-        response = super().partial_update(request, *args, **kwargs)
-        organizations = request.data.get("organizations")
-        if "organizations" in request.data:
-            self.update_condition_organizations(condition_id, organizations)
-        return response
+        with transaction.atomic():
+            response = super().partial_update(request, *args, **kwargs)
+            organizations = request.data.get("organizations")
+            if "organizations" in request.data:
+                self.update_condition_organizations(condition_id, organizations)
+            return response
 
     def destroy(self, request, *args, **kwargs):
         condition = self.get_object()
         condition_id = condition.id
-        MonitorConditionOrganization.objects.filter(monitor_condition_id=condition_id).delete()
-        condition.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        with transaction.atomic():
+            MonitorConditionOrganization.objects.filter(monitor_condition_id=condition_id).delete()
+            condition.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
     def update_condition_organizations(self, condition_id, organizations):
         """更新条件的组织"""
